c1627a77ae553e31300daf043122dff371411fcd
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-07-21 19:20:10 +0300

    banner image

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'network/api_request.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(ProviderScope(child: MyApp()));
 }
 
@@ -13,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -24,7 +29,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends ConsumerWidget {
-
   //ignore: top_level_function_literal_block
   final _fetchBanner = FutureProvider((ref) async {
     var result = await fetchBanner();
@@ -37,45 +41,97 @@ class MyHomePage extends ConsumerWidget {
     return result;
   });
 
-
   @override
   Widget build(BuildContext context,
       T Function<T>(ProviderBase<Object, T> provider) watch) {
     var bannerApiResult = watch(_fetchBanner);
     var featureImgApiResult = watch(_fetchFeatureImg);
 
-
     return Scaffold(
-      body: Column(children: [
-        featureImgApiResult.when(
-            data: (featureImages) =>
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    CarouselSlider(items: featureImages.map((e) =>
-                        Builder(builder: (context) =>
-                            Container(child: Image(
-                              image: NetworkImage(e.featureImgUrl),
-                              fit: BoxFit.cover,),),)).toList(),
-                      options: CarouselOptions(
-                        autoPlay: true,
-                        enlargeCenterPage: true,
-                        initialPage: 0,
-                        viewportFraction: 1
-                      ),
-                    ),
-                    Row(crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        children: [
+          //Feature
+          featureImgApiResult.when(
+              data: (featureImages) => Stack(
+                    alignment: Alignment.center,
                     children: [
-                      Icon(Icons.navigate_before,size: 45,color: Colors.white,),
-                      Icon(Icons.navigate_next,size: 45,color: Colors.white,),
-                    ],)
-                  ],
-                ),
-          loading: ()=> const Center(child: CircularProgressIndicator(),) ,
-          error: (error,stack) => Center(child: Text('$error'),)
-        )
-      ],),
+                      CarouselSlider(
+                        items: featureImages
+                            .map((e) => Builder(
+                                  builder: (context) => Container(
+                                    child: Image(
+                                      image: NetworkImage(e.featureImgUrl),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ))
+                            .toList(),
+                        options: CarouselOptions(
+                            autoPlay: true,
+                            enlargeCenterPage: true,
+                            initialPage: 0,
+                            viewportFraction: 1),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.navigate_before,
+                            size: 45,
+                            color: Colors.white,
+                          ),
+                          Icon(
+                            Icons.navigate_next,
+                            size: 45,
+                            color: Colors.white,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+              loading: () => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+              error: (error, stack) => Center(
+                    child: Text('$error'),
+                  )),
+          Expanded(
+            child: bannerApiResult.when(
+                loading: () => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                error: (error, stack) => Center(
+                      child: Text('$error'),
+                    ),
+                data: (bannerImages) => ListView.builder(
+                    itemCount: bannerImages.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Image(
+                                image: NetworkImage(
+                                    bannerImages[index].bannerImgUrl),
+                                fit: BoxFit.cover),
+                            Container(
+                              color: Color(0XFFFFFF),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Text(
+                                  bannerImages[index].bannerText,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    })),
+          )
+        ],
+      ),
     );
   }
 }
