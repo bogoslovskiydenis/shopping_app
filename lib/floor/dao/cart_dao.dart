92c24d5fd939c279faff76d3de37a6533fa0bab4
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-08-05 13:21:15 +0300

    add CartDAO

---
+++ lib/floor/dao/cart_dao.dart
@@ -0,0 +1,30 @@
+import 'package:floor/floor.dart';
+import 'package:shopping_app/floor/entity/cart_product.dart';
+
+@dao
+abstract class CartDAO {
+  //select uid
+  @Query('SELECT * FROM Cart WHERE uid=:uid')
+  Stream<List<Cart>> getAllItemInCartByUid(String uid);
+
+  //select uid / productId
+  @Query('SELECT * FROM Cart WHERE uid=:uid and productId=:id ')
+  Future<Cart> getItemInCartByUid(String uid, int id);
+
+  //delete uid
+  @Query('DELETE FROM Cart WHERE uid=:uid')
+  Stream<List<Cart>> clearCartByUid(String uid);
+
+  //update uid
+  @Query('UPDATE Cart SET uid=:uid')
+  Future<void> updateUidCart(String uid);
+
+  @insert
+  Future<void> insertCart(Cart product);
+
+  @update
+  Future<int> updateCart(Cart product);
+
+  @delete
+  Future<int> deleteCart(Cart product);
+}
